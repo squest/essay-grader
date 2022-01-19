97b3fe3dc3e43eafa3378b07ea884115935f0c7b
Sabda PS <sabdaps@gmail.com>
2022-01-20 02:02:20 +0700

    check essay

--- app/src/app/core.clj
+++ app/src/app/core.clj
@@ -1,6 +1,35 @@
-(ns app.core)
+(ns app.core
+  (:require [clojure.string :as cs]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn listify
+  [words]
+  (-> (cs/upper-case words)
+      (cs/replace #"[^\w\s]" " ")
+      (cs/split #" ")))
+
+(def good (-> (listify "Lorem ipsum dolor sit amet consectetur adipisicing elit Error possimus")
+              set))
+
+(def bad (-> (listify "Cum dolor enim excepturi libero nostrum placeat possimus quo totam unde voluptate")
+             set))
+
+(defn check-one
+  "Given an essay, count the frequency of good words and bad words"
+  [essay]
+  {:good (->> (listify essay)
+              (keep good)
+              count)
+   :bad  (->> (listify essay)
+              (keep bad)
+              count)})
+
+(defn check-two
+  [essay]
+  (let [new-essay (-> (cs/upper-case essay)
+                      (cs/replace #"[^\w\s]" " "))]
+    {:good (->> (vec good)
+                (filter #(cs/includes? new-essay %))
+                count)
+     :bad  (->> (vec bad)
+                (filter #(cs/includes? new-essay %))
+                count)}))
